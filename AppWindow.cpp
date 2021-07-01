f9bc323db2750560916385337277f2b6391c40b8
Kiwi <bruceakimiranda@gmail.com>
2021-07-01 10:44:48 +0800

    Created my first triangle assignment

--- AppWindow.cpp
+++ AppWindow.cpp
@@ -42,10 +42,9 @@ void AppWindow::onCreate()
 	// Temporary create triangle
 	vertex list_triangle[] =
 	{
-		{-0.5f, -0.5f, 0.0f,  -0.3f, -0.4f, 0.0f,   1.0f, 0.0f, 0.0f,   0.0f, 1.0f, 0.0f},
-		{-0.5f,  0.5f, 0.0f,  -0.25f, 0.8f, 0.0f,   0.0f, 1.0f, 0.0f,   0.0f, 0.0f, 1.0f},
-		{ 0.5f, -0.5f, 0.0f,   0.7f,  0.0f, 0.0f,   0.0f, 0.0f, 1.0f,   1.0f, 0.0f, 0.0f},
-		{ 0.5f,  0.5f, 0.0f,   0.9f,  0.7f, 0.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f}
+		{-0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   0.0f, 1.0f, 0.0f},
+		{ 0.0f,  0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   0.0f, 0.0f, 1.0f},
+		{ 0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   1.0f, 0.0f, 0.0f}
 	};
 	UINT size_list = ARRAYSIZE(list_triangle);
 
--- VertexShader.hlsl
+++ VertexShader.hlsl
@@ -1,7 +1,6 @@
 struct VS_INPUT
 {
 	float4 pos: POSITION;
-	float4 pos1: POSITION1;
 	float3 color: COLOR;
 	float3 color1: COLOR1;
 };
@@ -21,7 +20,7 @@ cbuffer constant: register(b0)
 VS_OUTPUT vsmain(VS_INPUT input)
 {
 	VS_OUTPUT output;
-	output.pos = lerp(input.pos, input.pos1, (sin(m_time / 1000.0f) + 1.0f) / 2.0f);
+	output.pos = input.pos;
 	output.color = input.color;
 	output.color1 = input.color1;
 	return output;
